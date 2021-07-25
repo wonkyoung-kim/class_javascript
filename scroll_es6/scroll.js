286f269bd59b0c0329a9ab61d5356b51429e3229
wkyoung <wwoj0205@gmail.com>
2021-07-25 13:36:49 +0900

    세로 스크롤 위치에 따른 버튼 활성화

--- scroll_es6/scroll.js
+++ scroll_es6/scroll.js
@@ -6,10 +6,32 @@
     scrollTop은 animatie()메서드 안에서 키값으로 활용(해당 스크롤 위치로 이동)
 */
 
+var pos1 = $('#box1').offset().top;
+var pos2 = $('#box2').offset().top;
+var pos3 = $('#box3').offset().top;
+var pos4 = $('#box4').offset().top;
+
 $(window).on('scroll', function(e){
     var scroll = $(e.currentTarget).scrollTop();
-    var posBox2 = $('#box2').offset().left;
+    var posBox2 = $('#box2').offset().top;
     $('h1').text(scroll);
+
+    if(scroll >= pos1){
+        $('#navi li').removeClass('on');
+        $("#navi li").eq(0).addClass('on');
+    }
+    if(scroll >= pos2){
+        $('#navi li').removeClass('on');
+        $("#navi li").eq(1).addClass('on');
+    }
+    if(scroll >= pos3){
+        $('#navi li').removeClass('on');
+        $("#navi li").eq(2).addClass('on');
+    }
+    if(scroll >= pos4){
+        $('#navi li').removeClass('on');
+        $("#navi li").eq(3).addClass('on');
+    }
 });
 
 $('#navi li').on('click', function(e){
@@ -19,6 +41,4 @@ $('#navi li').on('click', function(e){
     var targetPos = $(target).offset().top;
     $('html, body').stop().animate({scrollTop: targetPos}, 1000);
 
-    $('#navi li').removeClass('on');
-    $(e.currentTarget).addClass('on');
 })
